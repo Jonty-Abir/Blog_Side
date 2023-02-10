54898116935453bd19a396184ebc8789116cde8f
Abir Santra <abirrens@gmail.com>
2023-02-10 14:03:52 +0530

    Final_Submition

---
+++ instance/instance.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3000";
+// const baseUrl = "https://employ-management-system.vercel.app";
+const instance = axios.create({
+  baseURL: baseUrl,
+});
+
+export { instance };
+
